8cfc99f1d8a44dae6f2b85f5ff1975588495026f
TheAntiFlash <yahya.bin.naveed@gmail.com>
2024-03-02 22:33:53 +0500

    Implemented payment gatway with stripe

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import SignIn from './components/Auth/signin';
 import SignUp from './components/Auth/signup';
 import AuthDetails from './components/AuthDetails';
+import Checkout from './components/Payment/Stripe';
+import PaymentSuccess from './components/Payment/PaymentSuccess';
 import {
   BrowserRouter as Router,
   Routes,
@@ -17,6 +19,8 @@ function App() {
           <Route path="/Home" element={<AuthDetails />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/" element={<SignIn />} />
+          <Route path='/pay' element={<Checkout/>}/>
+          <Route path='success' element={<PaymentSuccess/>}/>
         </Routes>
       </Router>
   );
