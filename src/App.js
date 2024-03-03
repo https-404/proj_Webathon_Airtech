9f37079746734de032aad1d293f56aedd9943388
https-404 <areeshali@gmail.com>
2024-03-03 13:04:49 +0500

    RBA

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import SignIn from './components/Auth/signin';
 import SignUp from './components/Auth/signup';
-import AuthDetails from './components/AuthDetails';
+import HomePage from './components/HomePage';
 
 import {
   BrowserRouter as Router,
@@ -15,7 +15,7 @@ function App() {
   return (
       <Router>
         <Routes>
-          <Route path="/Home" element={<AuthDetails />} />
+          <Route path="/Home" element={<HomePage />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/" element={<SignIn />} />
         </Routes>
