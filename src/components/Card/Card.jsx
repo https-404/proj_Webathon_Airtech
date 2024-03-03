8e8f3620148f4ce983ce2f61dddfd7c0206b20ce
TheAntiFlash <yahya.bin.naveed@gmail.com>
2024-03-03 13:52:28 +0500

    final

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,11 +1,23 @@
 import MyMaps from "../GoogleMaps/GoogleMaps"
 import "./Card.css"
+import React from "react";
+import { useEffect, useState } from "react";
 function PackageCard(props) {
+    var weatherData = {}
+    var [weather, setWeather] = useState('')
+    const url=`http://api.weatherapi.com/v1/current.json?q=${props.lat},${props.lng}&key=12181df4bc424428a9782003240303`
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        setWeather(data.current.condition.text)
+    })
     return(
         <div className="card">
              <MyMaps lat={props.lat} lng={props.lng}/>
              <h2 className="card-title">{props.name}</h2>
              <p className="card-text">{props.status}</p>
+             <p className="card-text">{weather}</p>
         </div> 
     )
 }
